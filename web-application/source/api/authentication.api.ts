9b15180020ecb269c99e7e69894a788a7e9b4ae1
PlamoPhotos <plamodb@gmail.com>
2026-05-06 23:54:17 -0700

    Check magic link token on authentication page.

--- web-application/source/api/authentication.api.ts
+++ web-application/source/api/authentication.api.ts
@@ -10,6 +10,8 @@ import { API_URL } from '../constants';
 /**
  * If the user does not have an account, one will be created. Next, the user will
  * be emailed a "magic link" to sign into the application.
+ * 
+ * @param emailAddress The email address to sign in with.
  */
 export async function signInOrCreateAccount(emailAddress : string) {
   const response = await fetch(
@@ -28,6 +30,31 @@ export async function signInOrCreateAccount(emailAddress : string) {
   if (response.status !== 200) {
     throw new Error('Unable to sign or create a new account');
   }
+}
+
+/**
+ * Someone who has just logged in has a link with a token, use this to validate
+ * that token, after which a jwt will be assigned to the session.
+ * 
+ * @param token The token to authenticate.
+ */
+export async function authenticateAccount(token : string) : Promise<boolean> {
+  const params = new URLSearchParams({ token });  
+  const response = await fetch(
+    `${API_URL}/authentication/authenticate?${params}`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }
+  );
+
+  if (response.status !== 200) {
+    throw new Error('Unable to authenticate account.');
+  }
+
+  return response.status === 200;
 }
 
 // #endregion Authentication
--- web-application/source/views/sign-in/AuthenticationView.tsx
+++ web-application/source/views/sign-in/AuthenticationView.tsx
@@ -1,8 +1,16 @@
 // #region Imports
 
+/* React */
+import { useSearchParams, useNavigate } from 'react-router-dom';
+
 /* Classnames */
 import classnames from 'classnames';
-import { useSearchParams } from 'react-router-dom';
+
+/* Telestream */
+import { PageHeader } from 'components/PageHeader';
+import { ViewContainer } from 'components/ViewContainer';
+import { useEffect } from 'react';
+import { authenticateAccount } from 'api/authentication.api';
 
 // #endregion Imports
 
@@ -13,9 +21,23 @@ export function AuthenticationView() {
   // #region Authenticate
 
   const [searchParameters] = useSearchParams();
+  const navigate = useNavigate();
   const token = searchParameters.get('token');
 
-  console.log(token);
+  if (!token) {
+    navigate('/sign-in');
+  }
+
+  useEffect(
+    () => {
+      if (!token) {
+        return;
+      }
+
+      authenticateAccount(token);
+    },
+    [token]
+  );
 
   // #endregion Authenticate
 
@@ -24,16 +46,11 @@ export function AuthenticationView() {
       'mx-auto h-full md:w-lg pt-4',
       'flex flex-col gap-2'
     )}>
-      <span className='pc-page-header'>Authenticating</span>
+      <PageHeader label="Authenticating" />
 
-      <div className    = {classnames(
-        'flex-1 p-8',
-        'bg-(--tan-500)',
-        'border-4 border-shadow-500 border-b-0 rounded-t-xl',
-        'shadow-(--pc-shadow)'
-      )}>
-        Verifying Authentication        
-      </div>
+      <ViewContainer className="flex-1">
+        Checking authentication status
+      </ViewContainer>
     </div>
   )
 }
