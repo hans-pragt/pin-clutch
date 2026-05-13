69b4c1929ef7fddfdce430e25c71e6e0f3a9ab60
PlamoPhotos <plamodb@gmail.com>
2026-05-12 23:41:21 -0700

    Use a library for http status codes.

--- rest-api/source/api/users/users.ts
+++ rest-api/source/api/users/users.ts
@@ -3,6 +3,9 @@
 /* Express */
 import express, { Request, Response } from 'express';
 
+/* Status Codes */
+import { StatusCodes, getReasonPhrase } from 'http-status-codes';
+
 /* Clutch */
 import { stytchClient } from '../../authentication';
 import { User } from './types';
@@ -19,13 +22,25 @@ router.use(express.json());
 // #region Routes
 
 /**
- * Get user information for the authenticated user that is making the request. 
+ * Get user information for the authenticated user that is making the request.
+ * 
+ * @returns When the request comes from an authenticated user, various information
+ * about that user is returned. If getting the user information is not possible,
+ * a status message is returned.
  */
 router.get(
   '/me',
-  async (request : Request, response : Response<User>) => {
+  async (request : Request, response : Response<User | string>) => {
     const jwt = request.session.jwt;
     const stytchResponse = await stytchClient.sessions.authenticate({ session_jwt: jwt });
+
+    if (stytchResponse.status_code !== StatusCodes.OK) {
+      response
+        .status(StatusCodes.UNAUTHORIZED)
+        .send(getReasonPhrase(StatusCodes.UNAUTHORIZED))
+      return;
+    }
+
     response.json({
       id: stytchResponse.user.user_id
     });
--- rest-api/source/express-session.d.ts
+++ rest-api/source/express-session.d.ts
@@ -1,5 +1,15 @@
+// #region Imports
+
+/* Express-Session */
 import 'express-session';
 
+// #endregion Imports
+
+/**
+ * Extends the session object to add a jwt field. While I was hoping the library
+ * would offer a simpler way of doing this (using generics perhaps) this appears
+ * to be the recommended way.
+ */
 declare module 'express-session' {
 
   /**
