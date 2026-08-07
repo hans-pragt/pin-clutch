123d8514bf193be2c11dcb510e2ba18879a83130
PlamoPhotos <plamodb@gmail.com>
2026-08-06 23:56:31 -0700

    Add and integrate a close icon.

--- web-application/source/views/pins/components/SearchPanel.tsx
+++ web-application/source/views/pins/components/SearchPanel.tsx
@@ -7,9 +7,10 @@ import { useState } from 'react';
 import { InputGroup } from 'components/input/InputGroup';
 import { InputGroupAddOn } from 'components/input/InputGroupAddOn';
 import { InputGroupField } from 'components/input/InputGroupField';
-
-import SearchIcon from '@icons/monotone/search.svg?react';
 import { Button } from 'components/button/Button';
+
+import CloseIcon from '@icons/monotone/close.svg?react';
+import SearchIcon from '@icons/monotone/search.svg?react';
 
 // #endregion Imports
 
@@ -27,13 +28,25 @@ export function SearchPanel() {
     <div className="p-4">
       <InputGroup>
         <InputGroupAddOn>
-          <SearchIcon />
+          <SearchIcon className="size-6" />
         </InputGroupAddOn>
         <InputGroupField 
           name      = "search-query"
           value     = {query}
           onChange  = {event => setQuery(event.target.value)}
         />
+        {
+          query &&
+          <InputGroupAddOn position="end">
+            <Button 
+              size      = "icon-sm"
+              kind      = "ghost"
+              onClick   = {() => setQuery('')}
+            >
+              <CloseIcon />
+            </Button>
+          </InputGroupAddOn>
+        }
       </InputGroup>
     </div>
   );
